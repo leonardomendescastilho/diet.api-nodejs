8ffe6dc07659eb0ea0b0029945e421cf67b91009
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2024-09-11 11:45:17 -0300

    feat(migrations): create migrations table for users and meals

---
+++ database/migrations/20240911125805_meals.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('meals', (table) => {
+		table.uuid('id').primary();
+		table.string('description').notNullable();
+		table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+		table.boolean('diet').notNullable();
+		table.uuid('session_id').notNullable();
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTable('meals');
+}
