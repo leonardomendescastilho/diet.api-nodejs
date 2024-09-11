717850c21e5bac29b327da5bde645fa68685ee75
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2024-09-11 11:50:59 -0300

    feat(plugins) add a user route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify';
 import cookie from '@fastify/cookie';
+import { usersRoute } from './routes/users';
 
 const app = fastify();
 
 app.register(cookie);
+app.register(usersRoute, {
+	prefix: '/users',
+});
 
 export default app;
