ca7c865cddb57f21f1a7dd0c05d5c08b8a6d2206
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2024-09-11 11:52:36 -0300

    feat(interfaceTables): create users-meals interface table to use inside knex/types/tables

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,7 +1,21 @@
 import knex from 'knex';
+// import { UUID } from 'node:crypto';
 
 declare module 'knex/types/tables' {
 	export interface Tables {
-		users: {};
+		users: {
+			id: string;
+			name: string;
+			email: string;
+			session_id: string;
+		};
+	}
+
+	export interface Meals {
+		id: string;
+		description: string;
+		created_at: Date;
+		diet: boolean;
+		session_id: string;
 	}
 }
