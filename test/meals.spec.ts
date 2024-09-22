62ce6825eb3c22136878297f2b9765519c9d9dab
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2024-09-22 20:24:56 -0300

    feat(test): add a test to get the list of all meals and a meal by id

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -8,16 +8,18 @@ describe('Meals routes', () => {
 		await app.ready();
 	});
 
+	// beforeEach(async () => {
+	// 	console.log('Unlocking migrations...');
+	// 	execSync('npm run knex migrate:unlock');
+	// 	console.log('Rolling back migrations...');
+	// 	execSync('npm run knex migrate:rollback --all');
+	// 	console.log('Running migrations...');
+	// 	execSync('npm run knex migrate:latest');
+	// 	console.log('Migrations completed.');
+	// });
+
 	afterAll(async () => {
 		await app.close();
-	});
-
-	beforeEach(async () => {
-		console.log('Rolling back migrations...');
-		execSync('npm run knex migrate:rollback --all');
-		console.log('Running migrations...');
-		execSync('npm run knex migrate:latest');
-		console.log('Migrations completed.');
 	});
 
 	it.skip('should be able to create a new meal', async () => {
@@ -32,7 +34,58 @@ describe('Meals routes', () => {
 			.expect(201);
 	});
 
-	it('should be to delete a meal', async () => {
+	it.skip('should be able to list all meals', async () => {
+		await supertest(app.server)
+			.post('/meals')
+			.set('Cookie', 'session_id=123')
+			.send({
+				name: 'café com pão com ovo',
+				description: 'café comum brasileiro',
+				diet: true,
+			})
+			.expect(201);
+
+		await supertest(app.server)
+			.post('/meals')
+			.set('Cookie', 'session_id=123')
+			.send({
+				name: 'café com pão assado',
+				description: 'café comum brasileiro',
+				diet: false,
+			})
+			.expect(201);
+
+		const result = await supertest(app.server)
+			.get('/meals')
+			.set('Cookie', 'session_id=123')
+			.expect(200);
+	});
+
+	it.skip('should be a ble to get a meal by id', async () => {
+		await supertest(app.server)
+			.post('/meals')
+			.set('Cookie', 'session_id=123')
+			.send({
+				name: 'café com pão com ovo',
+				description: 'café comum brasileiro',
+				diet: true,
+			})
+			.expect(201);
+
+		const result = await supertest(app.server)
+			.get('/meals')
+			.set('Cookie', 'session_id=123');
+
+		const { id } = result.body[0];
+		console.log(id);
+
+		await supertest(app.server)
+			.get(`/meals/meal/${id}`)
+			.set('Cookie', 'session_id=123')
+			.expect(200);
+	});
+
+	it.skip('should be to delete a meal', async () => {
 		await supertest(app.server)
 			.post('/meals')
 			.set('Cookie', 'session_id=123')
@@ -49,7 +102,6 @@ describe('Meals routes', () => {
 
 		const { id } = result.body[0];
 
-		console.log(id);
 		await supertest(app.server)
 			.delete(`/meals/meal/${id}`)
 			.set('Cookie', 'session_id=123')
